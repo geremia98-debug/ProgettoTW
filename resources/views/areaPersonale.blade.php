f09c16cd05eab8dd857a1c0d808d4d509990586b
geremia98-debug <s1097385@studenti.univpm.it>
2023-08-28 17:57:54 +0200

    commit

---
+++ resources/views/areaPersonale.blade.php
@@ -0,0 +1,39 @@
+<body>
+    <header>
+        @include('partials.navbar')
+    </header>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area Personale Utente</title>
+    <style>
+        /* Aggiungi qui i tuoi stili CSS */
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Benvenuto nell'Area Personale</h1>
+    </header>
+    <section id="user-data">
+        <h2>I Tuoi Dati Personali</h2>
+        <div>
+            <p><strong>Nome:</strong> <span id="user-name"></span></p>
+            <p><strong>Cognome:</strong> <span id="user-lastname"></span></p>
+            <!-- Aggiungi altre informazioni qui -->
+        </div>
+    </section>
+    <footer>
+        <p>&copy; 2023 Nome della Tua Azienda</p>
+    </footer>
+    
+    <script>
+
+        // Popola i campi dei dati personali con i valori dell'utente
+        document.getElementById("user-name").textContent = userData.name;
+        document.getElementById("user-lastname").textContent = userData.lastname;
+        // Aggiungi altre assegnazioni per gli altri campi
+        
+        // Puoi utilizzare JavaScript per richiedere dati dal server o da un database reale
+    </script>
+</body>
+</html>
---
+++ resources/views/modificautente.blade.php
@@ -0,0 +1,36 @@
+<head>
+    <title>Registrazione</title>
+</head>
+<body>
+    <h1>Registrazione</h1>
+
+    <form method="POST" action="">
+        <label>Inserisci il nuovo nome:</label>
+        <input type="text" name="nome" required><br>
+
+        <label>Inserisci il nuovo cognome:</label>
+        <input type="text" name="cognome" required><br>
+
+        <label>Modifica la data di nascita:</label> 
+        <input type="date" name="data_nascita" required><br>
+
+        <label>Scegli la tua nuova occupazione:</label>
+        <select name="occupazione">
+            <option value="studente">Studente</option>
+            <option value="lavoratore">Lavoratore</option>
+            <option value="altro">Altro</option>
+        </select><br>
+
+        <label>Inserisci la tua email:</label>
+        <input type="email" name="email" required><br>
+
+        <label>Inserisci la nuova Password:</label>
+        <input type="password" name="password" required><br>
+
+        <label>Conferma la nuova Password:</label>
+        <input type="password" name="conferma_password" required><br>
+
+        <input type="submit" value="Registrati">
+    </form>
+</body>
+
