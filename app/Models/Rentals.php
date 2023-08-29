04bde44126f9c7fa734bebced18161ae100a87b2
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2023-08-29 11:18:42 +0200

    first commit

---
+++ app/Models/Rentals.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rentals extends Model
+{
+    protected $table = 'noleggi';
+    use HasFactory;
+}
