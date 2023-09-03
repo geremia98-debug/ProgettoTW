b72714980159ed7abf43c6990e59eaf538c6f78f
geremia98-debug <s1097385@studenti.univpm.it>
2023-09-03 16:33:56 +0200

    route commit

--- routes/web.php
+++ routes/web.php
@@ -17,25 +17,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-// Route::get('/home', function () {
-//     return view('home');
-// });
-
-// Route::get('/chi-siamo', function () {
-//     return view('ChiSiamo');
-// });
-
-// Route::get('/catalogo-auto', function() {
-//     return view('catalogo');
-// });
 
 Route::get('/registrazione', function() {
     return view('registrazione');
 });
 
-// Route::get('/area-personale', function() {
-//     return view('areaPersonale');
-// });
 
 Route::get('/modifica-utente', function() {
     return view('modificautente');
@@ -47,11 +33,13 @@ Route::get('/chi-siamo', [ChiSiamoController::class, 'index']);
 Route::get('/area-personale', [AreaPersonaleController::class, 'index']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/catalogo', [CatalogoController::class, 'index']);
-
-// Route::get('/login', [LoginController::class, 'login']);
-// Route::get('/registrazione', [RegistrazioneController::class, 'registrazione']);
-
 Route::post('/salva-dati', 'DatiController@salva')->name('registrazione');
-
-
+Route::get('/area-personale/{username}', [UserController::class, 'areaPersonale'])->middleware('auth');
+Route::get('/users', [app\Http\Controllers\UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [app\Http\Controllers\UserController::class, 'create'])->name('users.create');
+Route::post('/users', [app\Http\Controllers\UserController::class, 'store'])->name('users.store');
+Route::get('/users/{user}', [app\Http\Controllers\UserController::class, 'show'])->name('users.show');
+Route::get('/users/{user}/edit', [app\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
+Route::put('/users/{user}', [app\Http\Controllers\UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{user}', [app\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
 
