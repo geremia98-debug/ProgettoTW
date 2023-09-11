0a97ed25d63b20149d32ee5ae24fb9805c189750
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2023-09-11 11:09:46 +0200

    modified:   app/Http/Controllers/CarController.php

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -34,4 +34,66 @@ class CarController extends Controller
         // Passa i risultati alla vista
         return view('catalog.index', compact('cars'));
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'targa' => 'required|unique:cars',
+            'marca' => 'required',
+            'modello' => 'required',
+            'cilindrata' => 'required|integer',
+            'prezzo' => 'required|integer',
+            'posti' => 'required|integer',
+            'descrizione' => 'required',
+        ]);
+    
+        $car = new Car();
+    
+        $car->plate = $request->input('targa');
+        $car->brand = $request->input('marca');
+        $car->model = $request->input('modello');
+        $car->displacement = $request->input('cilindrata');
+        $car->price = $request->input('prezzo');
+        $car->seats = $request->input('posti');
+        $car->description = $request->input('descrizione');
+    
+    
+        $car->save();
+
+
+    }
+
+    public function show(Car $car)
+{
+    return view('cars.show', compact('car'))->with('success', 'Nuova vettura registrata con successo');
+}
+
+public function edit(Car $car)
+{
+    return view('cars.edit', compact('user'));
+}
+
+public function update(Request $request, Car $car)
+{
+    $car->plate = $request->input('targa');
+    $car->brand = $request->input('marca');
+    $car->model = $request->input('modello');
+    $car->displacement = $request->input('cilindrata');
+    $car->price = $request->input('prezzo');
+    $car->seats = $request->input('posti');
+    $car->description = $request->input('descrizione');
+
+    $user->update();
+
+}
+
+public function destroy(Car $car)
+{
+    $car->delete();
+    return redirect()->route('cars.index')->with([
+        'success' => "La vettura {$car->brand} {$car->model} targata {$car->plate} è stata rimossa."
+    ]);
+}
+
+
 }
